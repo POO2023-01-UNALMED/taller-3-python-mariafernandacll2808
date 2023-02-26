5f61ca2184fc9b33b2839a078d3c75a2b86ea708
mariafernandacll2808 <124931004+mariafernandacll2808@users.noreply.github.com>
2023-02-26 17:18:32 -0500

    Intento 2

--- televisores/control.py
+++ televisores/control.py
@@ -20,6 +20,9 @@ class Control:
     def volumenDown(self):
         self._tv.volumenDown()
 
+    def setCanal(self,canal):
+        self._tv.setCanal(canal)    
+
     def enlazar(self,tv):
         self._tv = tv
         tv.setControl(self)
